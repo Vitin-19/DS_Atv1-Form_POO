fbbd0eab3db0a5c3892e5bf50d8315162e7f8c09
Vitor Paiva Nunes de Paula <brdepaulav@gmail.com>
2025-03-10 17:36:27 -0300

    Some developments

--- Pessoa.java
+++ Pessoa.java
@@ -1,9 +1,19 @@
 public class Pessoa {
     String nome;
     int idade;
-    String email;
+    private String email;
 
-    public Pessoa(){
-        
+    public Pessoa(String nome, int idade, String email){
+        this.nome = nome;
+        this.idade = idade;
+        setEmail(email);
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email.toLowerCase();
     }
 }
